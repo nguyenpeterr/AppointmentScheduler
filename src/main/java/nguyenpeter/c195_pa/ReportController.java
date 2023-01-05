55f495732e6b02160d0b535e43e7cffd01a8126b
peternguyen <peterdsd1@yahoo.com>
2023-01-05 10:33:30 -0800

    Update reports page to reflect two table views (contact sched and customers). Add total customer count on the bottom

--- src/main/java/nguyenpeter/c195_pa/ReportController.java
+++ src/main/java/nguyenpeter/c195_pa/ReportController.java
@@ -20,49 +20,66 @@ public class ReportController implements Initializable {
 
 
     @FXML
-    private TableColumn<?, ?> column1;
-
+    private TableColumn<?, ?> apptIdCol;
     @FXML
-    private TableColumn<?, ?> column2;
-
+    private Label comboLabel;
     @FXML
-    private TableColumn<?, ?> column3;
-
+    private TableView<?> contactScheduleTableView;
     @FXML
-    private TableColumn<?, ?> column4;
-
+    private TableColumn<?, ?> createDateCol;
     @FXML
-    private TableColumn<?, ?> column5;
-
+    private TableColumn<?, ?> createdByCol;
     @FXML
-    private TableColumn<?, ?> column6;
-
+    private TableColumn<?, ?> custAddrCol;
     @FXML
-    private TableColumn<?, ?> column7;
-
+    private TableColumn<?, ?> custIdCol;
     @FXML
-    private TableColumn<?, ?> column8;
-
+    private TableColumn<?, ?> custNameCol;
     @FXML
-    private TableColumn<?, ?> column9;
-
+    private TableColumn<?, ?> custPhoneCol;
+    @FXML
+    private TableColumn<?, ?> custPostalCol;
+    @FXML
+    private TableColumn<?, ?> custStateCol;
+    @FXML
+    private TableColumn<?, ?> custViewCustIdCol;
+    @FXML
+    private TableView<?> customersTableView;
+    @FXML
+    private TableColumn<?, ?> descCol;
+    @FXML
+    private TableColumn<?, ?> endDateCol;
+    @FXML
+    private TableColumn<?, ?> endTimeCol;
+    @FXML
+    private TableColumn<?, ?> lastUpdateCol;
     @FXML
     private RadioButton rButtonByCountry;
-
     @FXML
     private RadioButton rButtonByMonth;
-
     @FXML
     private RadioButton rButtonByType;
-
     @FXML
     private RadioButton rButtonContactSched;
-
     @FXML
     private ComboBox<?> reportComboBox;
-
     @FXML
     private ToggleGroup reportTG;
+    @FXML
+    private TableColumn<?, ?> startDateCol;
+    @FXML
+    private TableColumn<?, ?> startTimeCol;
+    @FXML
+    private TableColumn<?, ?> titleCol;
+    @FXML
+    private TableColumn<?, ?> typeCol;
+    @FXML
+    private TableColumn<?, ?> updatedByCol;
+    @FXML
+    private Label totalCustLabel;
+    @FXML
+    private Label intTotalCustLabel;
+
 
     @FXML
     void onCloseButton(ActionEvent event) throws IOException {
@@ -74,10 +91,25 @@ public class ReportController implements Initializable {
 
     @FXML
     void onRadioSelect(ActionEvent event) {
-
+        if(rButtonContactSched.isSelected()) {
+            comboLabel.setText("Contact ID");
+            contactScheduleTableView.setVisible(true);
+            customersTableView.setVisible(false);
+            totalCustLabel.setText("");
+            intTotalCustLabel.setText("");
+        }
+        if(rButtonByType.isSelected() || rButtonByMonth.isSelected() || rButtonByCountry.isSelected()) {
+            comboLabel.setText("Customer ID");
+            contactScheduleTableView.setVisible(false);
+            customersTableView.setVisible(true);
+            totalCustLabel.setText("Total Customers:");
+            intTotalCustLabel.setText("--");
+        }
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        comboLabel.setText("");
+        totalCustLabel.setText("");
+        intTotalCustLabel.setText("");
     }
 }
