335a086fadd04738d30831d1e4ca7524de9d4ee6
peternguyen <peterdsd1@yahoo.com>
2023-01-05 10:52:27 -0800

    Add a few javadoc comments

--- src/main/java/nguyenpeter/c195_pa/MainController.java
+++ src/main/java/nguyenpeter/c195_pa/MainController.java
@@ -17,131 +17,107 @@ import java.net.URL;
 import java.time.ZoneId;
 import java.util.ResourceBundle;
 
-public class MainController implements Initializable {
+/**
+ * @author Peter Nguyen
+ */
 
+/**
+ * MainController class is referenced to the main user interface of Main.fxml. This is the window that users will interact with
+ * after logging in.
+ *
+ */
+
+public class MainController implements Initializable {
+    /**
+     * Sets the stage and scene of the GUI page.
+     */
     Stage stage;
     Parent scene;
-
+    /**
+     * Table columns are labeled representing data from the database.
+     * Radio buttons are for users to select for viewing certain data
+     * Buttons are dedicated for specific tasks (adding, updating, deleting data, etc..)
+     */
     @FXML
     private Button addButton;
-
     @FXML
     private TableColumn<?, ?> addressCol_c;
-
     @FXML
     private TableColumn<?, ?> appointmentIdCol_a;
-
     @FXML
     private Rectangle appointmentRectangle;
-
     @FXML
     private TableView<?> appointmentTableView;
-
     @FXML
     private TableColumn<?, ?> contactCol_a;
-
     @FXML
     private TableColumn<?, ?> createDateCol_c;
-
     @FXML
     private TableColumn<?, ?> createdByCol_c;
-
     @FXML
     private TableColumn<?, ?> customerIdCol_a;
-
     @FXML
     private TableColumn<?, ?> customerIdCol_c;
-
     @FXML
     private TableColumn<?, ?> customerNameCol_c;
-
     @FXML
     private TableView<?> customerTableView;
-
     @FXML
     private Button deleteButton;
-
     @FXML
     private TableColumn<?, ?> descriptionCol_a;
-
     @FXML
     private Button dismissRectangleButton;
-
     @FXML
     private TableColumn<?, ?> divisionIdCol_c;
-
     @FXML
     private TableColumn<?, ?> endCol_a;
-
     @FXML
     private TableColumn<?, ?> lastUpdateCol_c;
-
     @FXML
     private TableColumn<?, ?> lastUpdatedByCol_c;
-
     @FXML
     private TableColumn<?, ?> locationCol_a;
-
     @FXML
     private Button logOutButton;
-
     @FXML
     private ToggleGroup apptcustTG;
-
     @FXML
     private TableColumn<?, ?> phoneCol_c;
-
     @FXML
     private TableColumn<?, ?> postalCodeCol_c;
-
     @FXML
     private RadioButton radioAllAppt;
-
     @FXML
     private RadioButton radioApptByMonth;
-
     @FXML
     private RadioButton radioApptByWeek;
-
     @FXML
     private ToggleButton toggleAppointmentButton;
-
     @FXML
     private ToggleButton toggleCustomerButton;
-
     @FXML
     private Label rectangleLabel;
-
     @FXML
     private Button reportsButton;
-
     @FXML
     private Label serverTimeZoneLabel;
-
     @FXML
     private Label serverTimeZone;
-
     @FXML
     private TableColumn<?, ?> startCol_a;
-
     @FXML
     private Label yourTimeZoneLabel;
-
     @FXML
     private Label timeZone;
-
     @FXML
     private TableColumn<?, ?> titleCol_a;
-
     @FXML
     private TableColumn<?, ?> typeCol_a;
-
     @FXML
     private Button updateButton;
-
     @FXML
     private TableColumn<?, ?> userIdCol_a;
-
     @FXML
     private ToggleGroup viewTG;
 
@@ -160,8 +136,12 @@ public class MainController implements Initializable {
         }
     }
 
+    /**
+     * User selection to view all appointments
+     * @param event when View All radio button is selected
+     */
     @FXML
-    void onAllAppointmentsRadio(ActionEvent event) {
+    void onViewAllRadio(ActionEvent event) {
 
     }
 
